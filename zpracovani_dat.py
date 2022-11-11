6ae6c8ea7f7df98bc73c63836d06c7be62ce302e
ivjel <jelinkova.ivana@gmail.com>
2022-11-11 14:50:50 +0100

    extrakce memory_usage

--- zpracovani_dat.py
+++ zpracovani_dat.py
@@ -6,7 +6,7 @@ výstupem by mělo být (pro každý kontejner):
     Datumová pole převeďte na UTC timestamp.  
 """ 
 
-import json
+import json, re
 import datetime
 
 
@@ -45,7 +45,35 @@ with open(fname) as file:
     json_data = json.loads(str_data)
     for data in json_data: 
         #print('memory usage: ', data['state']) # [0]['memory'][0]['usage'])
-        print(json_extract(data, 'memory'))
+        #print(json_extract(data, 'memory'))
         print('name: ', data["name"])
         print('status: ', data['status'])
         print('created at: ', data['created_at'])
+        for key, value in data.items(): 
+            if key == 'state': 
+                state_dict = (value)
+                try: 
+                    for key, value in state_dict.items(): 
+                        if key == 'memory': 
+                            memory_dict = value
+                            #print('memory: ', memory_dict, type(memory_dict))
+                            for key, value in memory_dict.items(): 
+                                if key == 'usage': 
+                                    memory_usage = value
+                                    print('memory usage: ', memory_usage)
+                            
+
+                #print(state_dict)
+                #for value in data['memory']: 
+                #    usage = value['usage']
+                #    print('memory usage: ', usage)
+                #if key == 'memory': 
+                 #   print(value)
+                except AttributeError: 
+                    print('expanded_devices')
+                    pass
+                """ 
+                for key in data['state']: 
+                    if key == 'memory': 
+                        print(key)
+                """ 
