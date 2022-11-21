50df60a0c35ca8801789ec2e8088c872610a0deb
ivjel <jelinkova.ivana@gmail.com>
2022-11-21 23:07:30 +0100

    better version

--- zpracovani_dat.py
+++ zpracovani_dat.py
@@ -11,6 +11,70 @@ import re
 import psycopg2
 
 
+def parsing_network(value): 
+    """extracting IP like – "address" : "127.0.0.1"."""
+    network_str = str(value)
+    addresses = []
+    addresses = re.findall("address': '([0-9a-z:.]*)'", network_str)
+    return addresses
+
+
+def parsing_memory_cpu(value): 
+    """Extracting memory and cpu usage from JSON. """
+    cpu_or_memory_dict = value
+    for key, value in cpu_or_memory_dict.items(): 
+            if key == 'usage': 
+                return value
+
+
+
+def add_device(data): 
+    """Parsing JSON file to small pieces."""
+    name = ''
+    cpu_usage = 0
+    memory_usage = 0
+    created_at = '-'
+    status = '-'
+    addresses = []
+
+    name = data['name']
+    status = data['status']
+    created_at = data['created_at']
+
+    for key, value in data.items(): 
+        if key == 'state': 
+            state_dict = value
+            try: 
+                for key, value in state_dict.items(): 
+                    if key == 'memory': 
+                        memory_usage = parsing_memory_cpu(value)
+                    
+                    elif key == 'cpu': 
+                        cpu_usage = parsing_memory_cpu(value)
+
+                    elif key == 'network': 
+                        addresses = parsing_network(value)
+
+            except AttributeError: 
+                print('expanded_device')
+    return name, status, created_at, memory_usage, cpu_usage, addresses
+
+
+
+def giving_data_to_sql(name, cpu_usage, memory_usage, created_at, status): 
+    """Sending data to sql""" 
+    cur.execute('''INSERT INTO Devices (name, CPU_usage, memory_usage, created_at, status) 
+        VALUES (%s, %s, %s, %s, %s)''', (name, cpu_usage, memory_usage, created_at, status))
+    cur.execute(f"SELECT id FROM Devices WHERE name = '{name}'")
+    device_id = cur.fetchone()[0]
+
+    for pocet_IP in range(len(addresses)): 
+        address = addresses.pop()
+        cur.execute('INSERT INTO IPs (device_id, IP) VALUES (%s, %s)', (device_id, address))
+
+    conn.commit()
+    return
+
 
 # Connect to postgres DB
 conn = psycopg2.connect('dbname=parsing_json user=postgres password=heslo')
@@ -52,59 +116,8 @@ with open(fname) as file:
     str_data = file.read()
     json_data = json.loads(str_data)
     for data in json_data: 
-        name = data['name']
-        status = data['status']
-        created_at = data['created_at']
-  
-        for key, value in data.items(): 
-            if key == 'state': 
-                state_dict = (value)
-                try: 
-                    for key, value in state_dict.items(): 
-                        if key == 'memory': 
-                            memory_dict = value
-                            for key, value in memory_dict.items(): 
-                                if key == 'usage': 
-                                    memory_usage = value
-                        
-                        elif key == 'cpu': 
-                            cpu_dict = value
-                            for key, value in cpu_dict.items(): 
-                                if key == 'usage': 
-                                    cpu_usage = value
-
-                        elif key == 'network': 
-                            # extrakce IP like – "address" : "127.0.0.1",
-                            network_str = str(value)
-                            addresses = []
-                            addresses = re.findall("address': '([0-9a-z:.]*)'", network_str)
-
-                            
-                except AttributeError: 
-                    print('expanded_devices')
-
-
-        #print()
-        # giving data to sql: 
-        cur.execute('INSERT INTO Devices (name, CPU_usage, memory_usage, created_at, status) VALUES (%s, %s, %s, %s, %s)', (name, cpu_usage, memory_usage, created_at, status))
-        cur.execute(f"SELECT id FROM Devices WHERE name = '{name}'")
-        #print(cur.fetchall())
-        device_id = cur.fetchone()[0]
-        #print(device_id)
-        #print(len(addresses), type(addresses), addresses)
-        for pocet_IP in range(len(addresses)): 
-            address = addresses.pop()
-            #print(address)
-            cur.execute('INSERT INTO IPs (device_id, IP) VALUES (%s, %s)', (device_id, address))
-        conn.commit()
-        
-        # nulovani hodnot pro expanded_devices:
-        name = ''
-        cpu_usage = 0
-        memory_usage = 0
-        created_at = '-'
-        status = '-'
-        addresses = []
+        name, status, created_at, memory_usage, cpu_usage, addresses = add_device(data)
+        giving_data_to_sql(name, cpu_usage, memory_usage, created_at, status)
 
 
 # close the communication with the postgreSQL
